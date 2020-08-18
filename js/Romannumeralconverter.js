7331652a29364db23115e3c741507c27dfd28f81
JJuanC <camiloflstudio@gmail.com>
2020-08-17 21:38:15 -0500

    trabajando css num-rom

--- js/Romannumeralconverter.js
+++ js/Romannumeralconverter.js
@@ -6,19 +6,7 @@ function getNumber() {
 function toRomanNumber(num) {
     let romanNum = "";
     let romanNumeral = [
-        "M",
-        "CM",
-        "D",
-        "CD",
-        "C",
-        "XC",
-        "L",
-        "XL",
-        "X",
-        "IX",
-        "V",
-        "IV",
-        "I"
+        "M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"
     ];
     let decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
     for (let i = 0; i < decimalValue.length; i++) {
